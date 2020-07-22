eb8c61b3c077873b061186c91fbdae989c31b878
sammydowds <sammydowds1993@gmail.com>
2020-07-21 23:53:10 -0500

    Added logo to tab

---
+++ src/components/MainComponent.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'; 
+import moment from 'moment';
+import { highlights } from './highlights'; 
+import {
+    Jumbotron, 
+    Container
+} from 'reactstrap'; 
+
+
+class Main extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: null, 
+            nbaStart: null, 
+        }; 
+    }
+
+    tickDown() {
+        let tick = this.state.count - 1; 
+        this.setState({count: tick}); 
+    }
+
+    componentDidMount() {
+        let now = moment(); 
+        let nba = moment('2020-07-30'); 
+        let delta = nba.diff(now, 'seconds'); 
+        this.setState({count: delta, nbaStart: nba}); 
+        setInterval(() => {
+            this.tickDown(); 
+        }, 1000); 
+    }
+
+    render () {
+        let totalSeconds = this.state.count; 
+        let days = Math.floor(totalSeconds/86400); 
+        totalSeconds = totalSeconds %= 86400; 
+        let hours = Math.floor(totalSeconds/3600); 
+        totalSeconds = totalSeconds %= 3600; 
+        let minutes = Math.floor(totalSeconds/60); 
+        let seconds = totalSeconds % 60; 
+        let highlight_info = highlights.map((highlight) => {
+            return(
+                <p>
+                    <a href={highlight[0]} target="_blank">{highlight[1]}</a>
+                </p>
+            )
+        })
+        return(
+            <div className='main-component' >
+                <Jumbotron className='bg-transparent' fluid>
+                    <Container fluid>
+                        <img src="heart.png" /> <img src='nB@.png' />
+                        <h1 className='display-2'>NBA is coming back in: </h1>
+                        <h3 className='display-3'>{days} days {hours} hours {minutes} minutes {seconds} seconds</h3>
+                        <h2>bby come back</h2>
+                        {highlight_info}
+                    </Container>
+                </Jumbotron>
+            <div>
+    </div>
+                
+            </div>
+        ); 
+    }
+
+}
+
+export default Main; 
---
+++ src/components/highlights.js
@@ -0,0 +1,8 @@
+export const highlights = [
+    ['https://www.youtube.com/watch?v=kIF8zcj_J4Y', 'Kyrie with the dagger'], 
+    ['https://www.youtube.com/watch?v=Y52sQoSAr6g', 'Steve Nash - best assists'], 
+    ['https://www.youtube.com/watch?v=AXvIvbzfPag', 'Lebron chase down blocks'], 
+    ['https://www.youtube.com/watch?v=cKD6Nwo0vjk', 'RIP HAMILTON (only the real ones know)'], 
+    ['https://www.youtube.com/watch?v=Tq_1j4z5-J4', 'Monta Ellis - never forget'], 
+    ['https://www.youtube.com/watch?v=x7AsDQYPIJk', 'Larry Bird - 2.0 (Adam Morrison)']
+]
